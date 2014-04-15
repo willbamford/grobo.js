70dc7269895c8d6e8b6b479f523a8320b6001987
Will Bamford <w.bamford@gmail.com>
2014-04-15 15:53:23 +0100

    Minor spacing tweaks.

--- js/logger.js
+++ js/logger.js
@@ -1,16 +1,20 @@
 define([], function () {
 
     var list = document.getElementById('logger');
+
     return {
+
         log: function (message) {
             console.log(message);
             var item = document.createElement('li');
             item.appendChild(document.createTextNode(message));
             list.appendChild(item);
         },
+
         info: function (message) {
             this.log('Info: ' + message);
         },
+
         err: function (e) {
             this.log('Error: ' + e);
         }
