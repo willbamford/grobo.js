01bd6b6eb5b7145ccd667fd2d05c7c36dae129f2
Will Bamford <w.bamford@gmail.com>
2014-04-16 12:11:59 +0100

    Add default update, draw and handleInput methods to state.

--- js/states/state.js
+++ js/states/state.js
@@ -6,8 +6,19 @@ define([], function () {
         EXCLUSIVE: 1,
 
         modality: null,
+        canvas: null,
+        stateFactory: null,
+        stateManager: null,
 
-        init: function () {
+        _initState: function (config) {
+            this.modality = config.modality || null;
+            this.canvas = config.canvas || null;
+            this.stateFactory = config.stateFactory || null;
+            this.stateManager = config.stateManager || null;
+        },
+
+        init: function (config) {
+            this._initState(config);
             return this;
         },
 
@@ -17,7 +28,13 @@ define([], function () {
 
         obscured: function () {},
 
-        revealed: function () {}
+        revealed: function () {},
+
+        update: function (delta) {},
+
+        draw: function () {},
+
+        handleInput: function (event) {}
     };
 
     return refState;
--- js/tests/spec/states/state-spec.js
+++ js/tests/spec/states/state-spec.js
@@ -8,21 +8,41 @@ define(['lib', 'states/state'], function (lib, refState) {
             state = lib.create(refState);
         });
 
-        it('should return "this" on init', function () {
-            var state2 = state.init();
+        it('should return self on initialisation', function () {
+            var state2 = state.init({});
             expect(state2).toEqual(state);
         });
 
         it('should provide default methods for "entered", "exiting", "obscured" and "revealed"', function () {
-            state.init();
+            state.init({});
             expect(typeof state.entered).toEqual('function');
             expect(typeof state.exiting).toEqual('function');
             expect(typeof state.obscured).toEqual('function');
             expect(typeof state.revealed).toEqual('function');
         });
 
-        it('should have a default modality of null', function () {
-            expect(state.modality).toBeNull();
+        it('should provide default methods for "update", "draw" and "handleInput"', function () {
+            state.init({});
+            expect(typeof state.update).toEqual('function');
+            expect(typeof state.draw).toEqual('function');
+            expect(typeof state.handleInput).toEqual('function');
+        });
+
+        it('should be able to initialise with modality, canvas, stateFactory and stateManager', function () {
+            var modality = refState.EXCLUSIVE,
+                canvas = 'canvas',
+                stateFactory = 'stateFactory',
+                stateManager = 'stateManager'
+            state.init({
+                modality: modality,
+                canvas: canvas,
+                stateFactory: stateFactory,
+                stateManager: stateManager
+            });
+            expect(state.modality).toEqual(modality);
+            expect(state.canvas).toEqual(canvas);
+            expect(state.stateFactory).toEqual(stateFactory);
+            expect(state.stateManager).toEqual(stateManager);
         });
     });
 });
