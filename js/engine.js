f77d056af182d6f6c9f88af1da81d3ae16b491d4
Will Bamford <w.bamford@gmail.com>
2014-04-19 09:14:05 +0100

    Add dependency on RequireJS to bower config. Move animation frame polyfill to module.

--- js/engine.js
+++ js/engine.js
@@ -1,51 +1,54 @@
-define(['fps', 'logger', 'polyfills/function-bind'], function (fps, logger) {
+define(
+    ['fps', 'logger', 'polyfills/function-bind', 'polyfills/animation-frame'],
+    function (fps, logger) {
 
-    var refEngine = {
+        var refEngine = {
 
-        init: function (stateManager) {
-            var self = this;
-            this.stateManager = stateManager;
-            this.tickCount = 0;
-            this.requestId = null;
-            this.isRunning = false;
-            this.lastTime = null;
-
-            fps.init(120, function (framesPerSecond) {
-                logger.info(framesPerSecond);
-            });
-
-            return this;
-        },
-
-        start: function () {
-            if (!this.isRunning) {
-                this.requestId = window.requestAnimationFrame(this.tick.bind(this));
-                this.isRunning = true;
-            }
-        },
-
-        stop: function () {
-            if (this.isRunning) {
-                window.cancelAnimationFrame(this.requestId);
+            init: function (stateManager) {
+                var self = this;
+                this.stateManager = stateManager;
+                this.tickCount = 0;
+                this.requestId = null;
                 this.isRunning = false;
+                this.lastTime = null;
+
+                fps.init(120, function (framesPerSecond) {
+                    logger.info(framesPerSecond);
+                });
+
+                return this;
+            },
+
+            start: function () {
+                if (!this.isRunning) {
+                    this.requestId = window.requestAnimationFrame(this.tick.bind(this));
+                    this.isRunning = true;
+                }
+            },
+
+            stop: function () {
+                if (this.isRunning) {
+                    window.cancelAnimationFrame(this.requestId);
+                    this.isRunning = false;
+                }
+            },
+
+            tick: function () {
+                var currentTime = new Date(),
+                    delta = 0;
+                this.tickCount += 1;
+                if (this.lastTime) {
+                    delta = currentTime - this.lastTime;
+                }
+                this.stateManager.tick(delta);
+                this.lastTime = currentTime;
+
+                this.requestId = window.requestAnimationFrame(this.tick.bind(this));
+
+                fps.tick(delta);
             }
-        },
+        };
 
-        tick: function () {
-            var currentTime = new Date(),
-                delta = 0;
-            this.tickCount += 1;
-            if (this.lastTime) {
-                delta = currentTime - this.lastTime;
-            }
-            this.stateManager.tick(delta);
-            this.lastTime = currentTime;
-
-            this.requestId = window.requestAnimationFrame(this.tick.bind(this));
-
-            fps.tick(delta);
-        }
-    };
-
-    return refEngine;
-});
+        return refEngine;
+    }
+);
